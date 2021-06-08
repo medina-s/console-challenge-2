3265d40d718da243f0e7a56c71780add81e5141d
Sydykanova <sydykanova.medina@gmail.com>
2021-06-08 15:57:09 -0400

    first push

---
+++ index.js
@@ -0,0 +1,7 @@
+
+// BRONZE : Create a function that takes in an array and returns the sum of all the element of an array.
+ 
+// SILVER : If the array does not have any elements in it, return a string message for invalid arguments.
+ 
+// GOLD : Return the sums of the elements that are multiples of three ONLY. If there are no multiples of three, 
+// return a string message for invalid arguments
